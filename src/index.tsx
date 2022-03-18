85d70bf0eff3199ad81f9951f17333ebb200f052
eder <ederoliveirabarreto@gmail.com>
2022-03-17 22:03:57 -0300

    Add format button

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,23 @@
-import * as esbuild from "esbuild-wasm";
-import { useEffect, useRef, useState } from "react";
-import ReactDOM from "react-dom";
-import { fetchPlugin } from "./plugins/fetch-plugin";
-import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
+import 'bulmaswatch/superhero/bulmaswatch.min.css';
+import * as esbuild from 'esbuild-wasm';
+import { useState, useEffect, useRef } from 'react';
+import ReactDOM from 'react-dom';
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
+import { fetchPlugin } from './plugins/fetch-plugin';
+import CodeEditor from './components/code-editor';
 
 const App = () => {
-  const [input, setInput] = useState("");
-  const ref = useRef<any>(null);
-  const iframe = useRef<any>(null);
+  const ref = useRef<any>();
+  const iframe = useRef<any>();
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
 
   const startService = async () => {
     ref.current = await esbuild.startService({
       worker: true,
-      wasmURL: "https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm",
+      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
     });
   };
-
   useEffect(() => {
     startService();
   }, []);
@@ -28,17 +30,18 @@ const App = () => {
     iframe.current.srcdoc = html;
 
     const result = await ref.current.build({
-      entryPoints: ["index.js"],
+      entryPoints: ['index.js'],
       bundle: true,
       write: false,
       plugins: [unpkgPathPlugin(), fetchPlugin(input)],
       define: {
-        "process.env.NODE_ENV": '"production"',
-        global: "window",
+        'process.env.NODE_ENV': '"production"',
+        global: 'window',
       },
     });
 
-    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, "*");
+    // setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
   };
 
   const html = `
@@ -50,12 +53,12 @@ const App = () => {
           window.addEventListener('message', (event) => {
             try {
               eval(event.data);
-            } catch(err) {
+            } catch (err) {
               const root = document.querySelector('#root');
               root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
               console.error(err);
             }
-          }, false)
+          }, false);
         </script>
       </body>
     </html>
@@ -63,22 +66,21 @@ const App = () => {
 
   return (
     <div>
-      <textarea
-        onChange={(e) => setInput(e.target.value)}
-        value={input}
-        name=""
+      <CodeEditor
+        initialValue="const a = 1;"
+        onChange={(value) => setInput(value)}
       />
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <iframe
-        ref={iframe}
-        title="preview"
-        sandbox="allow-scripts"
-        srcDoc={html}
-      ></iframe>
+      <pre>{code}</pre>
+      <iframe ref={iframe} sandbox="allow-scripts" srcDoc={html} />
     </div>
   );
 };
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.querySelector('#root'));
