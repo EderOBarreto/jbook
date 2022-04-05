84a5166fd01b85833b293dbcd006d7d4f9329dfc
eder <ederoliveirabarreto@gmail.com>
2022-04-05 10:47:27 -0300

    Add redux

---
+++ src/state/action-creators/index.ts
@@ -0,0 +1,46 @@
+import { CellTypes } from "./../cell";
+import { ActionType } from "../action-types";
+import {
+  DeleteCellAction,
+  InsertCellBeforeAction,
+  MoveCellAction,
+  UpdateCellAction,
+  Direction,
+} from "../actions";
+
+export const updateCell = (id: string, content: string): UpdateCellAction => {
+  return {
+    type: ActionType.UPDATE_CELL,
+    payload: { content, id },
+  };
+};
+
+export const deleteCell = (id: string): DeleteCellAction => {
+  return {
+    type: ActionType.DELETE_CELL,
+    payload: id,
+  };
+};
+
+export const moveCell = (id: string, direction: Direction): MoveCellAction => {
+  return {
+    type: ActionType.MOVE_CELL,
+    payload: {
+      id,
+      direction,
+    },
+  };
+};
+
+export const insertCellBefore = (
+  id: string,
+  type: CellTypes
+): InsertCellBeforeAction => {
+  return {
+    type: ActionType.INSERT_CELL_BEFORE,
+    payload: {
+      id,
+      type,
+    },
+  };
+};
